d6fbe075faeddc09b59d021eb4603eaf0a485c0a
klausendias <50660288+klausendias@users.noreply.github.com>
2019-05-24 10:42:55 +0100

    Python Week One

--- MarksPercentage.py
+++ MarksPercentage.py
@@ -3,12 +3,15 @@ Chemistry=100
 Mathematics=100
 Total= Physics+Chemistry+Mathematics
 Per= Total*100/450
+print()
 print("Physics: ",Physics)
 print("Chemistry: ",Chemistry)
 print("Mathematics: ",Mathematics)
 print()
-print("----------------")
+print("--------------------")
 print()
 print("Total: ",Total)
-print("----------------")
+print()
+print("--------------------")
+print()
 print("Percentage: ",Per,"%")
