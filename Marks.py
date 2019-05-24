a0bd5a1a5db5296f742e617e08436f6ab8b40c3d
klausendias <klaus_dias1@hotmail.co.uk>
2019-05-24 10:17:17 +0100

    Add three new python files

---
+++ Marks.py
@@ -0,0 +1,5 @@
+Physics=80
+Chemistry=100
+Mathematics=100
+Total= Physics+Chemistry+Mathematics
+print("The result is",Total)
---
+++ MarksPercentage.py
@@ -0,0 +1,14 @@
+Physics=80
+Chemistry=100
+Mathematics=100
+Total= Physics+Chemistry+Mathematics
+Per= Total*100/450
+print("Physics: ",Physics)
+print("Chemistry: ",Chemistry)
+print("Mathematics: ",Mathematics)
+print()
+print("----------------")
+print()
+print("Total: ",Total)
+print("----------------")
+print("Percentage: ",Per,"%")
